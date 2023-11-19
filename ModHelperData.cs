7a44b8ef309fb9b20eceecda15c10c83fcddeeb6
DarkTerraYT <liamwolfguy@gmail.com>
2023-11-19 15:11:58 -0500

    Update Version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace Extension;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "37.3";
-    public const string Version = "1.1.0";
+    public const string Version = "1.1.1";
     public const string Name = "Customizable Bloon";
 
     public const string Description = "Like customizable tower but with a bloon instead!\n\nLimited for now but more features will be added later on.\n\nIf you want to change any displays/icons for the bloon simply download this source code and change the pngs and compile the mod yourself. \nYou'll need Visual Studio and .net 6.0. In the mod menu create a mod called CustomBloon and put the source code into the mod folder.\nYou can see where the mod sources are kept in the config for the Mod Helper.\nAfter that open the .sln file which should be called CustomBloon.sln and go to build and click on Build CustomBloon, Build Solution (which is used when you have multiple projects in the solution) or press Ctrl+B.\nMake sure you delete the original .dll first. ";
